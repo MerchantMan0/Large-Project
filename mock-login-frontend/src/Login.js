313ec29177dd718f353f32fae52bd1758da2e228
Noah <Noahporter86@gmail.com>
2026-03-27 12:16:21 -0400

    fix front end and package issues

--- mock-login-frontend/src/Login.js
+++ mock-login-frontend/src/Login.js
@@ -11,7 +11,7 @@ function Login() {
     setError("");
 
     try {
-      const response = await fetch("http://localhost:5000/auth/login", {
+      const response = await fetch("/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
